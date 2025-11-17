dfc94aec4a7e1a0870f4c6663737c1985697dad0
dudu-theman <dustanley07@gmail.com>
2025-11-16 21:40:37 -0600

    remove comment

--- backend/app.py
+++ backend/app.py
@@ -11,7 +11,7 @@ from werkzeug.utils import secure_filename
 
 load_dotenv()
 
-# ----------------- AWS SETUP -----------------
+
 AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
 AWS_REGION = os.getenv("AWS_REGION")
